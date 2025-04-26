2885f9dbc57092e40172ca114d803d94f4800b38
ankit-mangal-quoppo <ankit@quoppo.xyz>
2025-04-26 12:33:37 +0530

    broken: login not working

--- apps/login/src/app/login/route.ts
+++ apps/login/src/app/login/route.ts
@@ -65,6 +65,8 @@ async function loadSessions({
     ids: ids.filter((id: string | undefined) => !!id),
   });
 
+  console.log("loadSessions", response);
+
   return response?.sessions ?? [];
 }
 
@@ -72,9 +74,16 @@ const ORG_SCOPE_REGEX = /urn:zitadel:iam:org:id:([0-9]+)/;
 const ORG_DOMAIN_SCOPE_REGEX = /urn:zitadel:iam:org:domain:primary:(.+)/; // TODO: check regex for all domain character options
 const IDP_SCOPE_REGEX = /urn:zitadel:iam:org:idp:id:(.+)/;
 
+console.log("ORG_SCOPE_REGEX", ORG_SCOPE_REGEX);
+console.log("ORG_DOMAIN_SCOPE_REGEX", ORG_DOMAIN_SCOPE_REGEX);
+console.log("IDP_SCOPE_REGEX", IDP_SCOPE_REGEX);
+
 export async function GET(request: NextRequest) {
+  console.log("GET");
   const _headers = await headers();
   const { serviceUrl } = getServiceUrlFromHeaders(_headers);
+
+  console.log("serviceUrl", serviceUrl);
 
   const searchParams = request.nextUrl.searchParams;
 
@@ -89,6 +98,8 @@ export async function GET(request: NextRequest) {
       : samlRequestId
         ? `saml_${samlRequestId}`
         : undefined);
+
+  console.log("Final requestId:", requestId);
 
   const sessionId = searchParams.get("sessionId");
 
@@ -137,11 +148,14 @@ export async function GET(request: NextRequest) {
       authRequestId: requestId.replace("oidc_", ""),
     });
 
+    console.log("Auth Request:", authRequest);
+
     let organization = "";
     let suffix = "";
     let idpId = "";
 
     if (authRequest?.scope) {
+      console.log("Auth Request Scopes:", authRequest.scope);
       const orgScope = authRequest.scope.find((s: string) =>
         ORG_SCOPE_REGEX.test(s),
       );
@@ -150,9 +164,12 @@ export async function GET(request: NextRequest) {
         IDP_SCOPE_REGEX.test(s),
       );
 
+      console.log("Org Scope:", orgScope);
+      console.log("IDP Scope:", idpScope);
       if (orgScope) {
         const matched = ORG_SCOPE_REGEX.exec(orgScope);
         organization = matched?.[1] ?? "";
+        console.log("organization", organization);
       } else {
         const orgDomainScope = authRequest.scope.find((s: string) =>
           ORG_DOMAIN_SCOPE_REGEX.test(s),
